58218a3bbac279f5d966a186815f28bafe0eded1
camjmoore <cjmooredev@gmail.com>
2024-01-07 16:00:20 -0400

    configured server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./server.js');
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
