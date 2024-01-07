dfea1bd74aa63845b0caa2ef09ae21fb431210a4
camjmoore <cjmooredev@gmail.com>
2024-01-07 14:07:13 -0400

    migration - create resources table

---
+++ data/migrations/20240107175234_create-resources-table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('resources', function (resources) {
+    resources.increments('resource_id');
+    resources.string('resource_name').unique().notNullable();
+    resources.string('resource_description');
+    resources.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('resources');
+};
