cbdfb1c2709913bcfd8239dd18fd4d2c820678d1
camjmoore <cjmooredev@gmail.com>
2024-01-07 18:00:22 -0400

    project router get and post endpoints

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,29 @@
-// build your `/api/projects` router here
+const express = require('express');
+
+const router = express.Router();
+
+const Projects = require('./model.js');
+
+router.get('/', (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to get projects' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const projectData = req.body;
+
+  Projects.insert(projectData)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to create new project' });
+    });
+});
+
+module.exports = router;
