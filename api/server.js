cb4bbab787795a421da5e2c623d5e17bb046a476
camjmoore <cjmooredev@gmail.com>
2024-01-07 16:18:23 -0400

    set up express server and routing

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+// const projectsRouter = require('./project/router');
+// const resourcesRouter = require('./resource/router');
+// const tasksRouter = require('./task/router');
+
+// server.use('/api/projects', projectsRouter);
+// server.use('/api/resources', resourcesRouter);
+// server.use('/api/tasks', tasksRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'Server is running' });
+});
+
+module.exports = server;
