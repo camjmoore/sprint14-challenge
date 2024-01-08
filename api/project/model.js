7b7f3acabf1f58540e085b291ca50bd551a1dd00
camjmoore <cjmooredev@gmail.com>
2024-01-07 22:03:15 -0400

    project model outline

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,16 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+module.exports = {
+  get,
+  insert,
+};
+
+function get() {
+  //needs to be a boolean
+  return db('projects');
+}
+
+function insert(project) {
+  //needs to be a integer
+  return db('project').insert(project, 'project_id');
+}
