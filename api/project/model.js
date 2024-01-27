2ac6296e84d8660d08b16ee5c11fe371739209d0
camjmoore <cjmooredev@gmail.com>
2024-01-27 11:44:26 -0400

    implemented get and insert functions for project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,16 +1,30 @@
 const db = require('../../data/dbConfig');
+const mappers = require('../../data/mappings/mappers');
 
 module.exports = {
   get,
   insert,
 };
 
-function get() {
-  //needs to be a boolean
-  return db('projects');
+//breadcrumb: explicit async func declaration needed ?
+function get(id) {
+  let query = db('projects');
+
+  if (id) {
+    return query
+      .where('project_id', id)
+      .first()
+      .then((project) => (project ? mappers.retrieveProject(project) : null));
+    //if project exists, return the project, else return null
+  } else {
+    return query.then((projects) => {
+      return projects.map((project) => mappers.retrieveProject(project));
+    });
+  }
 }
 
 function insert(project) {
-  //needs to be a integer
-  return db('project').insert(project, 'project_id');
+  return db('projects')
+    .insert(project)
+    .then(([project_id]) => get(project_id));
 }
