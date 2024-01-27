00844f96ab5b77aa27a414fd950a39033f798166
camjmoore <cjmooredev@gmail.com>
2024-01-27 11:20:11 -0400

    basic relation mapping for retrieved and stored objects

---
+++ data/mappings/mappers.js
@@ -0,0 +1,60 @@
+module.exports = {
+  intToBool,
+  boolToInt,
+  storeProject,
+  storeResource,
+  storeTask,
+  retrieveProject,
+  retrieveResource,
+  retrieveTask,
+};
+
+function intToBool(int) {
+  return int === 1 ? true : false;
+}
+
+function boolToInt(bool) {
+  return bool === true ? 1 : 0;
+}
+
+function storeProject(project) {
+  return {
+    ...project,
+    completed: intToBool(project.project_completed),
+  };
+}
+
+function retrieveProject(project) {
+  return {
+    ...project,
+    completed: boolToInt(project.project_completed),
+  };
+}
+
+function storeResource(resource) {
+  return {
+    ...resource,
+    completed: boolToInt(resource.resource_completed),
+  };
+}
+
+function retrieveResource(resource) {
+  return {
+    ...resource,
+    completed: intToBool(resource.resource_completed),
+  };
+}
+
+function storeTask(task) {
+  return {
+    ...task,
+    completed: boolToInt(task.task_completed),
+  };
+}
+
+function retrieveTask(task) {
+  return {
+    ...task,
+    completed: intToBool(task.task_completed),
+  };
+}
