d9dd55b7b512ad7b3c3a78f6cdae89d10005a5eb
cam <cjmooredev@gmail.com>
2024-02-18 14:40:43 -0400

    call resources and projects routers in server

--- api/server.js
+++ api/server.js
@@ -4,12 +4,12 @@ const server = express();
 
 server.use(express.json());
 
-// const projectsRouter = require('./project/router');
-// const resourcesRouter = require('./resource/router');
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
 // const tasksRouter = require('./task/router');
 
-// server.use('/api/projects', projectsRouter);
-// server.use('/api/resources', resourcesRouter);
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
 // server.use('/api/tasks', tasksRouter);
 
 server.get('/', (req, res) => {
