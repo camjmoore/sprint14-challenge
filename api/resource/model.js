17546cd81de44d5ea0e648a893cb93fbe4a73e6d
cam <cjmooredev@gmail.com>
2024-02-18 14:10:35 -0400

    implemented get and insert for resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,18 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    get,
+    insert,
+};
+
+function get(id) {
+    let query = db('resources');
+
+    return id ? query.where('resource_id', id).first() : query;
+}
+
+function insert(resource) {
+    return db('resources')
+        .insert(resource)
+        .then(([resource_id]) => get(resource_id));
+}
