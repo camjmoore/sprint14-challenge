a66c4c7c3a0d1d4c27bc40838abf0a90280260b7
cam <cjmooredev@gmail.com>
2024-02-18 14:59:05 -0400

    call tasks router in server

--- api/server.js
+++ api/server.js
@@ -6,11 +6,11 @@ server.use(express.json());
 
 const projectsRouter = require('./project/router');
 const resourcesRouter = require('./resource/router');
-// const tasksRouter = require('./task/router');
+const tasksRouter = require('./task/router');
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/resources', resourcesRouter);
-// server.use('/api/tasks', tasksRouter);
+server.use('/api/tasks', tasksRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'Server is running' });
