fbe6c2ec4f2d21c8ce92954afbdda8ebf08e1855
cam <cjmooredev@gmail.com>
2024-02-18 15:01:11 -0400

    implemented task get and post endpoints

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,29 @@
-// build your `/api/tasks` router here
+const express = require('express');
+
+const router = express.Router();
+
+const Tasks = require('./model.js');
+
+router.get('/', (req, res) => {
+  Tasks.get()
+    .then((tasks) => {
+      res.status(200).json(tasks);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to get tasks' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const taskData = req.body;
+
+  Tasks.insert(taskData)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to create new task' });
+    });
+});
+
+module.exports = router;
