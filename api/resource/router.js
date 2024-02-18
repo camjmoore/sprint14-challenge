ab41a87c18e1054764102b258d689867b8a7a4c6
cam <cjmooredev@gmail.com>
2024-02-18 14:35:41 -0400

    resource get and post endpoints

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,30 @@
-// build your `/api/resources` router here
+const express = require('express');
+
+const router = express.Router();
+
+const Resources = require('./model.js');
+
+router.get('/', (req, res) => {
+  Resources.get()
+    .then((resources) => {
+      res.status(200).json(resources);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to get resources' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const resourceData = req.body;
+
+  Resources.insert(resourceData)
+    .then((resource) => {
+      res.status(201).json(resource);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to create new resource' });
+    });
+});
+
+module.exports = router;
+
